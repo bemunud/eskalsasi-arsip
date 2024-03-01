958befed2da262ba7eca78e05c7a7995498a6aab
phanorama <riandikafathurrochim@gmail.com>
2024-03-01 11:47:39 +0800

    initial : test

--- src/components/mainpage/section-two.tsx
+++ src/components/mainpage/section-two.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Junk(){
     return(
-        <div className="container bg-black w-full h-screen"></div>
+        <div className="container bg-green-400 w-full h-screen"></div>
     )
 }
