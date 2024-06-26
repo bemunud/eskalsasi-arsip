4f6b3ea2a1ce94f26f397782d7f0fefae79e8e6f
phanorama <riandikafathurrochim@gmail.com>
2024-06-26 23:52:34 +0800

    update : marquee

--- src/components/mainpage/blog.tsx
+++ src/components/mainpage/blog.tsx
@@ -15,34 +15,62 @@ export default async function BlogPreview() {
         <div className="md:container">
           <div className="md:container">
             <div className="md:container">
-              <div className="gap-5 mt-4 justify-evenly border-y border-black divide-y divide-black">
+              <div className="gap-3 mt-4 justify-evenly border-y border-black divide-y divide-black">
                 {posts.results.slice(0, 3).map((post: any) => (
-                  <div key={post.id} className="flex gap-8 py-6">
+                  <Link
+                    key={post.id}
+                    className="flex gap-8 py-3 flex-col lg:flex-row"
+                    href={`/blog/${post.properties.slug.rich_text[0].plain_text}`}
+                  >
                     <div
-                      className="bg-cover bg-center w-64 h-32"
+                      className="bg-cover bg-center w-full h-64 lg:w-64 lg:h-32 rounded-lg"
                       style={{
                         backgroundImage: `url(${post.cover.external.url})`,
                       }}
                     ></div>
 
-                    <div className="">
+                    <div className="flex flex-col justify-center items-start align-middle w-auto gap-1">
                       <h1 className="font-extrabold text-2xl text-center text-black">
                         {post.properties.Title.title[0].plain_text}
                       </h1>
                       {post.properties.Tags.multi_select.map((tag: any) => (
-                        <p key={tag.id}>{tag.name}</p>
+                        <div
+                          key={tag.id}
+                          className={
+                            tag.name === "kajian"
+                              ? "px-2 py-0.5 text-sm font-medium bg-green-200 text-green-600 border-green-400 border-2 rounded-lg"
+                              : tag.name === "warta"
+                              ? "px-2 py-0.5 text-sm font-medium bg-purple-200 text-purple-600 border-purple-400 border-2 rounded-lg"
+                              : tag.name === "berita"
+                              ? "px-2 py-0.5 text-sm font-medium bg-stone-200 text-stone-600 border-stone-400 border-2 rounded-lg"
+                              : ""
+                          }
+                        >
+                          {tag.name}
+                        </div>
                       ))}
-                      <p>{post.properties.slug.rich_text[0].plain_text}</p>
-                      <p>{post.properties.Date.created_time}</p>
+                      <div className="text-lg font-medium">
+                        {new Date(
+                          post.properties.Date.created_time
+                        ).toLocaleDateString("en-US", {
+                          year: "numeric",
+                          month: "long",
+                          day: "numeric",
+                        })}
+                      </div>
                     </div>
-                  </div>
+                  </Link>
                 ))}
               </div>
             </div>
           </div>
         </div>
+
         <Link href={"/blog"} className="flex items-center justify-center">
-          <Button className="rounded-xl px-12 py-6 font-medium">
+          <Button
+            className="rounded-xl px-12 py-6 font-medium"
+            variant={"esklasi"}
+          >
             Baca selengkapnya dari kami
           </Button>
         </Link>
--- src/components/mainpage/section2.tsx
+++ src/components/mainpage/section2.tsx
@@ -2,6 +2,8 @@ import { cn } from "@/lib/utils";
 import Marquee from "@/components/magicui/marquee";
 import { DEPARTEMEN } from "@/scripts";
 import Image from "next/image";
+import { Button } from "../ui/button";
+import Link from "next/link";
 
 const firstRow = DEPARTEMEN.slice(0, DEPARTEMEN.length / 2);
 const secondRow = DEPARTEMEN.slice(DEPARTEMEN.length / 2);
@@ -28,7 +30,13 @@ const ReviewCard = ({
       )}
     >
       <div className="flex flex-row items-center gap-2">
-        <Image className="rounded-full" width="32" height="32" alt="" src={img} />
+        <Image
+          className="rounded-full"
+          width="32"
+          height="32"
+          alt=""
+          src={img}
+        />
         <div className="flex flex-col">
           <figcaption className="text-sm font-medium">{name}</figcaption>
           <p className="text-xs font-medium">{username}</p>
@@ -41,14 +49,15 @@ const ReviewCard = ({
 
 export default function MarqueeSlider() {
   return (
-    <section className="w-full flex flex-col gap-4 container">
-      <h2 className="font-extrabold text-4xl text-center text-black">
+    <section className="w-full flex flex-col gap-2 container justify-center items-center">
+      <h2 className="font-extrabold text-3xl md:text-4xl text-center text-black">
         Lanskap Kementerian & Biro BEM Udayana
       </h2>
-      <h2 className="font-extrabold text-lg text-center text-slate-400">
-        Terdapat beberapa kementerian dan biro diberbagai bidang yang hadir di BEM Udayana saat ini
+      <h2 className="font-medium text-sm md:text-lg max-w-3xl text-balance text-center text-[#D4D7DB]">
+        Terdapat beberapa kementerian dan biro diberbagai bidang yang hadir di
+        BEM Udayana saat ini
       </h2>
-      <div className="relative flex h-full w-full flex-col items-center justify-center overflow-hidden py-12 bg-primary ">
+      <div className="relative flex h-full w-full flex-col items-center justify-center overflow-hidden py-5 bg-primary ">
         <Marquee pauseOnHover className="[--duration:20s]">
           {firstRow.map((review) => (
             <ReviewCard key={review.username} {...review} />
@@ -62,6 +71,11 @@ export default function MarqueeSlider() {
         <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-[#f6fafe]"></div>
         <div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-[#f6fafe]"></div>
       </div>
+      <Link href={"/tentang-bem"} className="flex items-center justify-center">
+        <Button className="rounded-xl px-12 py-6 font-medium" variant={"esklasi"}>
+          Cari tahu lebih lanjut
+        </Button>
+      </Link>
     </section>
   );
 }
