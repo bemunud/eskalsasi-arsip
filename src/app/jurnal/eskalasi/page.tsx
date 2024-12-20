a8dcef1f573fffe44478d485f34f53675d12f444
phanorama <riandikafathurrochim@gmail.com>
2024-12-20 23:09:09 +0800

    update-jurnal

--- src/app/jurnal/eskalasi/page.tsx
+++ src/app/jurnal/eskalasi/page.tsx
@@ -4,7 +4,7 @@ export default function ResponsiveEmbedIssuu() {
       {/* Wrapper dengan rasio default (16:9) */}
       <div className="relative pb-[100vh] md:pb-[75%] lg:pb-[56.25%]">
         <iframe
-          src="https://heyzine.com/flip-book/ddb411abe1.html"
+          src="https://heyzine.com/flip-book/c3fb00f2b5.html"
           className="absolute top-0 left-0 w-full h-full border-none"
           allowFullScreen
           title="Responsive Issuu Document"
