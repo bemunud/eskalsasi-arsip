b6b9098643c601a73e350bfd666aa7e487f01d5c
phanorama <riandikafathurrochim@gmail.com>
2024-12-20 19:32:32 +0800

    update

--- src/app/jurnal/eskalasi/page.tsx
+++ src/app/jurnal/eskalasi/page.tsx
@@ -1,21 +1,15 @@
 export default function ResponsiveEmbedIssuu() {
   return (
-    <div style={{ position: "relative", paddingBottom: "56.25%", height: 0 }}>
-      <iframe
-        src="https://heyzine.com/flip-book/ddb411abe1.html"
-        className="fp-iframe"
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          width: "100%",
-          height: "100%",
-          border: "none",
-        }}
-        frameBorder="0"
-        allowFullScreen
-        title="Responsive Issuu Document"
-      ></iframe>
+    <div className="relative w-full">
+      {/* Wrapper dengan rasio default (16:9) */}
+      <div className="relative pb-[100vh] md:pb-[75%] lg:pb-[56.25%]">
+        <iframe
+          src="https://heyzine.com/flip-book/ddb411abe1.html"
+          className="absolute top-0 left-0 w-full h-full border-none"
+          allowFullScreen
+          title="Responsive Issuu Document"
+        ></iframe>
+      </div>
     </div>
   );
 }
